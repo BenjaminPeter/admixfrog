38e73db754bba6021768d5fcde8394d892a95e46
benjamin_peter <benjamin_peter@eva.mpg.de>
2019-02-14 13:42:56 +0100

    build wo cython

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 from setuptools import setup#, find_namespace_packages
-from Cython.Build import cythonize
+from distutils.extension import Extension
+
+
+USE_CYTHON = False
+
+ext = '.pyx' if USE_CYTHON else '.c'
+
+extensions = [Extension("admixfrog",
+                        ["admixfrog/distributions"+ext,
+                         "admixfrog/hmm_updates"+ext
+                         ])]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
+
+    setup(setup_requires["pbr"],
+        ext_modules = extensions,
+          pbr=True)
 
 setup(setup_requires=["pbr"], 
-      ext_modules = cythonize(["admixfrog/*.pyx"], annotate=True),
+      ext_modules = extensions,#cythonize(["admixfrog/*.pyx"], annotate=True),
       pbr=True)
 
