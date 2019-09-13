27c93049335d2fc069bae81143341da76343c759
benjamin_peter <benjamin_peter@eva.mpg.de>
2019-09-13 17:59:44 +0200

    deubg map

--- admixfrog/admixfrog.py
+++ admixfrog/admixfrog.py
@@ -248,7 +248,7 @@ def load_admixfrog_data(states,
         raise ValueError("ambiguous input")
 
     #get ids of unique snps
-    breakpoint()
+    if 'map' in df: del df['map'] #weird error
     snp_ids = df[~df.index.duplicated()].groupby(df.index.names).ngroup()
     snp_ids = snp_ids.rename('snp_id')
     snp_ids = pd.DataFrame(snp_ids)                              
--- admixfrog/fwd_bwd.py
+++ admixfrog/fwd_bwd.py
@@ -130,6 +130,7 @@ def viterbi_single_obs(alpha0, trans_mat, emissions):
         backtrack[i] = np.argmax(aux_mat, 1)
 
     path = np.empty(n_steps, np.int8)
+    breakpoint()
     cursor = np.argmax(ll[-1])
     for i in range(n_steps - 1, -1, -1):
         cursor = backtrack[i, cursor]
