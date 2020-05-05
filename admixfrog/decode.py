fixed issue with overflow error with SNP with more than 128x coverage by a single library). Now program will abort

admixfrog/decode.py
@@ -9,7 +9,7 @@ from .log import log_
 @njit  # ('i8(i8, i8)')
 def get_hap_from_diploid(n_states, n_homo, est_inbreeding=False):
     n_states = n_states + n_homo if est_inbreeding else n_states
-    het2homo = np.zeros((n_states, 2), np.int8)
+    het2homo = np.zeros((n_states, 2), np.uint8)
     for s in range(n_homo):
         het2homo[s] = s
     for s1 in range(n_homo):

admixfrog/fwd_bwd.py
@@ -116,7 +116,7 @@ def viterbi_single_obs(alpha0, trans_mat, emissions):
     n_steps, n_states = emissions.shape
 
     ll = np.ones_like(emissions)
-    backtrack = np.zeros_like(emissions, np.int8)
+    backtrack = np.zeros_like(emissions, np.uint8)
 
     log_e = np.log(emissions)
     log_t = np.log(trans_mat)
@@ -129,7 +129,7 @@ def viterbi_single_obs(alpha0, trans_mat, emissions):
         ll[i] = np.max(aux_mat, 1)
         backtrack[i] = np.argmax(aux_mat, 1)
 
-    path = np.empty(n_steps, np.int8)
+    path = np.empty(n_steps, np.uint8)
     if ll.size == 0:
         return np.zeros(0, dtype=int)
     cursor = np.argmax(ll[-1])

admixfrog/utils.py
@@ -136,9 +136,11 @@ def data2probs(
                 ref_prior[ref_is_anc, i] += panc
                 ref_prior[ref_is_der, i] += pder
 
+        assert np.all(df.tref.values + df.talt.values < 256)
+
         P = Probs2(
-            O=np.array(df.talt.values, np.int8),
-            N=np.array(df.tref.values + df.talt.values, np.int8),
+            O=np.array(df.talt.values, np.uint8),
+            N=np.array(df.tref.values + df.talt.values, np.uint8),
             P_cont=np.zeros_like(df.talt.values)
             if cont_id is None
             else np.array(
@@ -169,9 +171,10 @@ def data2probs(
         alt_prior = snp_df[alt_ix].to_numpy() + prior_anc_alt[:, np.newaxis] + prior
         ref_prior = snp_df[ref_ix].to_numpy() + prior_anc_ref[:, np.newaxis] + prior
         #breakpoint()
+        assert df.tref.values + df.talt.values < 256
         P = Probs2(
-            O=np.array(df.talt.values, np.int8),
-            N=np.array(df.tref.values + df.talt.values, np.int8),
+            O=np.array(df.talt.values, np.uint8),
+            N=np.array(df.tref.values + df.talt.values, np.uint8),
             P_cont=0.
             if cont_id is None
             else np.array(