7367daf098aa6e5c01e5b5a9c716db7a39bef25b
benjamin_peter <benjamin_peter@eva.mpg.de>
2022-02-04 13:52:26 +0100

    draft fstats funs

---
+++ admixfrog/slug/fstats.py
@@ -0,0 +1,13 @@
+def calc_fstats(sfs, states, stats_to_calc=None):
+    """should calculate a table with all f3/f4 between source states and target"""
+    return fstats
+
+def calc_f3(sfs, states, stats_to_calc=None):
+    """should calculate a table with all f3 between source states and target"""
+    return fstats
+
+def calc_f4(sfs, states, stats_to_calc=None):
+    """should calculate a table with all f4 between source states and target"""
+    return fstats
+
+
