c6c5779b5928c453cb75e6bea462fccb4e3de024
benjamin_peter <benjamin_peter@eva.mpg.de>
2025-02-21 13:49:36 +0100

    updated docs

--- tests/test_slug.py
+++ tests/test_slug.py
@@ -36,26 +36,75 @@ def test_update_ftau():
     pass
 
 def test_slug_fwd_p_x():
-    pg = np.array([[0, .5, .5], [1, 0, 0]])
-    pa = np.array([.3, 1])
-    pc = np.array([0.3, 1])
+    """test example to calculate P(X|.)
+    - 3 SNPs, 
+    - SNP3 is flipped (alt = anc)
+    - psi is 100% der for all SNPs
+    - 4 Reads (2, 1, 1) with alleles (ref, alt, alt, ref)
+    - 2 Read groups (0, 1, 0, 1)
+    - contamination probs are (0.3, 0)
 
-    READS = np.array((0, 0, 1))
+
+    """
+
+    pg = np.array([[0, .5, .5], [1, 0, 0], [1/3, 1/3, 1/3]])
+    pa = np.array([1, 1, 1])
+    pc = np.array([0.3, 0])
+
+    READS = np.array((0, 1, 1, 0))
+    FLIPPED_READS = np.array((0, 0, 0, 1), 'bool')
     
-    n_reads = 3
+    n_reads = 4
     n_rgs = 2
-    n_snps = 2
-    READ2RG = np.array([0, 0, 1])
-    READ2SNP = np.array([0, 1, 1])
+    n_snps = 3
+    READ2RG = np.array([0, 1, 0, 1])
+    READ2SNP = np.array([0, 0, 1, 2])
 
-    bwd_x = bwd_p_o_given_x(READS, 0, 0)
-    bwd_g1 = bwd_p_one_o_given_g(bwd_x, pa, pc, READ2SNP,
-                                      READ2RG, n_reads)
+    bwd_x = bwd_p_o_given_x(READS, FLIPPED_READS, 0, 0)
+    #Pr(O|X, e=b=0); 1 if X is the same as READS, except for the flipped SNP
+    assert np.allclose(bwd_x[:,1] , (0,1,1,1))
+
+    bwd_g1 = bwd_p_one_o_given_g(bwd_x, pa, pc, READ2SNP, READ2RG, n_reads)
+    """Pr(O|G)
+        - Read1: we observe ref=anc allele, 30% chance cont; Pr(O|G=0) =  .3 *0 + .7 * 1 = 0.7
+        - Read2: we observe alt=der allele, 0% chance cont; Pr(O|G=0) =  .0 *0 + 1 * 0 = 0
+        - Read3: we observe alt=der allele, 30% chance cont; Pr(O|G=0) =  .3 *1 + .7 * 0 = 0.3
+        - Read4: we observe ref=der allele, 0% chance cont; Pr(O|G=0 (anc)) = 0
+    """
+    assert np.allclose(bwd_g1[:,0] , (0.7, 0, 0.3, 0))
+
     bwd_g = bwd_p_all_o_given_g(bwd_g1, READ2SNP, n_snps)
+    assert np.allclose(bwd_g[0] , bwd_g1[0] * bwd_g1[1])
+    assert np.allclose(bwd_g[1:] , bwd_g1[2:])
 
-    px_cont = fwd_p_x_cont(pa, READ2SNP)
+
+    px_cont = fwd_p_x_cont(pa, READ2SNP) #all 1 in this case
+    assert np.allclose(px_cont[:, 1], 1)
     px_nocont = message_fwd_p_x_nocont(pg, bwd_g, bwd_g1, READ2SNP)
     res = fwd_p_x(px_cont, px_nocont, pc, READ2RG)
+
+    """fwd probabilities:
+     - reads1, 2: 
+        - case1: both are endogenous (0.7 * 1), 
+                P(X_1=0 X_2=0| .) = pg[0,1] * .5 * .5 = 1/8
+                P(X_1=0 X_2=1| .) = pg[0,1] * .5 * .5 + pg[1,1] * 0  = 1/8
+                P(X_1=1 X_2=0| .) = pg[0,1] * .5 * .5 + pg[1,1] * 0  = 1/8
+                P(X_1=1 X_2=1| .) = pg[0,1] * .5 * .5 + pg[1,1] * 1  = 5/8
+        - case2: only 2 endogenous (0.3 * 1)
+                P(X_1=0 X_2=0| .) = 0
+                P(X_1=0 X_2=1| .) = 0
+                P(X_1=1 X_2=0| .) = pg[0,1] * 0.5 = 0.25
+                P(X_1=1 X_2=1| .) = pg[0,1] * 0.5 + pg[1,1] * 1  = 3/4
+        total:
+                P(X_1=0 X_2=0| .) = 1/8 * 7/10 = 7/80
+                P(X_1=0 X_2=1| .) = 7/80
+                P(X_1=1 X_2=0| .) = 13/80
+                P(X_1=1 X_2=1| .) = 53/80 
+        ergo
+                p(X_1 = 0) = 14/80 = 0.175
+                p(X_2 = 0) = 14/80 = 0.175
+    """
+
 
     assert res[0, 1] == .3 * .3 + .75 * .7
     assert res[1, 1] == .3 
@@ -131,13 +180,13 @@ def test_error_est():
         b = 0.001
     )
     controller = SlugController(update_eb=True,  update_ftau=False, update_cont=False)
-    update_pars_reads(pars, data, controller)
+    update_pars(pars, data, controller)
     print( f'e : {pars.prev_e} -> {pars.e}')
     print( f'b : {pars.prev_b} -> {pars.b}')
     assert pars.e == 1/2.
     assert pars.b == 1/2.
 
-    update_pars_reads(pars, data, controller)
+    update_pars(pars, data, controller)
     assert pars.prev_ll <= pars.ll
     assert pars.e == pars.prev_e
     assert pars.b == pars.prev_b
@@ -166,7 +215,7 @@ def test_cont_est():
     )
     controller = SlugController(update_eb=False,  update_ftau=False,
                                 update_cont=True)
-    pars = update_pars_reads(pars, reads, controller)
+    pars = update_pars(pars, reads, controller)
     assert pars.cont[0] == 0
     assert pars.cont[2] == 1
     assert pars.cont[1] == 0.5
