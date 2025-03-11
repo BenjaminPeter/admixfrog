379ed7d2121b6fbd8f0327c72edcd27cc46f22f6
Arev Sümer <75009964+arevsumer@users.noreply.github.com>
2025-03-11 16:03:37 +0100

    Update fstats.py
    added sterr to summarize_f4()

--- admixfrog/slug/fstats.py
+++ admixfrog/slug/fstats.py
@@ -237,7 +237,7 @@ def summarize_pi(pis):
 
 
 def summarize_f4(df):
-    cols = ["sex_chrom", "A", "B", "C", "D", "f4", "sd"]
+    cols = ["sex_chrom", "A", "B", "C", "D", "f4", "sd", "sterr"]
     if len(df) == 0:  # empty case
         return pd.DataFrame(columns=cols)
     f4s = summarize_f(df, stat="f4", pops=["A", "B", "C", "D"])
